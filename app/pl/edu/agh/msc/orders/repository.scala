88cad636a4ca33552a3f6697abadc7cf337d7632
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2018-02-20 20:38:39 +0100

    Add orders repo stub

--- app/pl/edu/agh/msc/orders/repository.scala
+++ app/pl/edu/agh/msc/orders/repository.scala
@@ -1,5 +1,43 @@
 package pl.edu.agh.msc.orders
 
-class OrdersRepository {
+import java.time.LocalDateTime
+import java.util.UUID
+import javax.inject.{ Inject, Singleton }
+import java.util.concurrent.Future
+
+import pl.edu.agh.msc.products.ProductId
+import pl.edu.agh.msc.utils.SlickTypeMappings
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.ExecutionContext
+
+@Singleton class OrdersRepository @Inject() (dbConfigProvider: DatabaseConfigProvider) extends SlickTypeMappings {
+  private val dbConfig = dbConfigProvider.get[JdbcProfile]
+  protected val profile = dbConfig.profile
+  import dbConfig.db
+  import profile.api._
+
+  private case class OrderRow(
+    status:        Int,
+    date:          LocalDateTime,
+    fullName:      String,
+    streetAddress: String,
+    zipCode:       String,
+    city:          String,
+    country:       String,
+    id:            UUID
+  )
+
+  private case class LineItemRow(
+    orderId:   UUID,
+    productId: Long,
+    amount:    Int,
+    price:     BigDecimal
+  )
+
+  def insert(order: Order)(implicit ec: ExecutionContext): Future[Unit] = {
+    ???
+  }
 
 }
