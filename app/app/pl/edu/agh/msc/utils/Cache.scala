4094dca6f59a1bd3ad657253b60566fac412d3c8
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2018-08-15 19:00:15 +0200

    Delay cache

--- app/app/pl/edu/agh/msc/utils/Cache.scala
+++ app/app/pl/edu/agh/msc/utils/Cache.scala
@@ -1,12 +1,13 @@
 package pl.edu.agh.msc.utils
 
+import akka.actor.ActorSystem
 import cats.instances.future._
 import cats.data.OptionT
 import javax.inject.Inject
 import play.api.cache.AsyncCacheApi
 
-import scala.concurrent.duration.Duration
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
+import scala.concurrent.{ ExecutionContext, Future, Promise }
 import scala.reflect.ClassTag
 
 trait KeyFormat[A] {
@@ -19,14 +20,21 @@ object KeyFormat {
   implicit def defaultKeyFormat[A]: KeyFormat[A] = _.toString
 }
 
-class Cache @Inject() (cache: AsyncCacheApi) {
+class Cache @Inject() (cache: AsyncCacheApi, actorSystem: ActorSystem) {
+
+  private def delay(duration: FiniteDuration)(implicit ec: ExecutionContext): Future[Unit] = {
+    val promise = Promise[Unit]()
+    val r: Runnable = () => promise.success(())
+    actorSystem.scheduler.scheduleOnce(duration, r)
+    promise.future
+  }
 
   def cached[K: KeyFormat, V: ClassTag](
     namespace:  String,
     expiration: Duration = Duration.Inf
   )(key: K)(fetch: K => Future[V])(implicit ec: ExecutionContext): Future[V] = {
     val formattedKey = asKey(key, namespace)
-    cache.getOrElseUpdate(formattedKey, expiration)(fetch(key))
+    delay(100.millis).flatMap(_ => cache.getOrElseUpdate(formattedKey, expiration)(fetch(key)))
   }
 
   def cachedOpt[K: KeyFormat, V: ClassTag](
